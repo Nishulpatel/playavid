dc5f0d41bf0077b2fb26bf088bd424ec52fd29b8
Nishulpatel <161936160+Nishulpatel@users.noreply.github.com>
2025-08-08 12:31:54 +0530

    resolve build error

--- src/components/comments-list.tsx
+++ src/components/comments-list.tsx
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { useEffect, useState } from "react"
-import { formatDistanceToNow } from "date-fns"
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 type Comment = {
   id: string
